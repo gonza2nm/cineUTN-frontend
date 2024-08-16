9b09b71bd77a859d2f7a25867041668405efaa6e
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-08-15 23:19:40 -0300

    creacion de navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent{
+  isMenuOpen = false;
+  isAuth = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    
+  }
+}
