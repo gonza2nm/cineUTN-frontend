98731ee3b21dbd680b92468bfadc6593e6ffe45a
Santino S <santinosimbel@gmail.com>
2024-08-21 23:16:40 -0300

    uso Get con http, uso de formGruop para login formulario y validaciones

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(private service: LoginService) { }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+
+  }
+
+
+  loadOneUser() {
+    this.service.getUser(1).subscribe(
+      response => console.log(response),
+      error => console.log(error)
+    )
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl(''),
+  })
 
 }
