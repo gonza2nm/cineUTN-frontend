480c5b5e910d713c143bd123cd5ec3b989b09ac6
Santino S <santinosimbel@gmail.com>
2024-09-09 17:24:05 -0300

    creacion de cinema.service e intento inicial de getAllCinemas()

---
+++ src/app/cinemas/cinema.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http/index.js';
+import { Observable } from 'rxjs';
+import { Cinema } from '../interfaces/interfaces.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CinemaService {
+
+  readonly apiUrl = 'http://localhost:3000/api/cinemas';
+
+  constructor(private http: HttpClient) { }
+
+  //Hace una peticion get al URL de la api, y espera un observable que va a emitira un arreglo de elemento de tipo cinema 
+  getAllCinemas(): Observable<Cinema[]> {
+    return this.http.get<Cinema[]>(this.apiUrl);
+  }
+}
