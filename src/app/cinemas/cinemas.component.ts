69dac7f245b0fa90d8a4c02fcb33d392e142b186
Santino S <santinosimbel@gmail.com>
2024-09-09 17:48:16 -0300

    creacion de metodo loadCinemas para cinemas.component.ts

--- src/app/cinemas/cinemas.component.ts
+++ src/app/cinemas/cinemas.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cinema } from '../interfaces/interfaces.js';
+import { CinemaService } from './cinema.service.js';
 
 @Component({
   selector: 'app-cinemas',
   templateUrl: './cinemas.component.html',
   styleUrls: ['./cinemas.component.css']
 })
-export class CinemasComponent {
+export class CinemasComponent implements OnInit {
 
+  cinemas: Cinema[] = [];
+
+  constructor(private cinemaService: CinemaService) { }
+
+  ngOnInit(): void {
+    this.loadCinemas();
+  }
+
+  loadCinemas() {
+    this.cinemaService.getAllCinemas().subscribe(
+      (data: Cinema[]) => {
+        this.cinemas = data;
+      },
+      (error) => {
+        console.error('Error getting cinemas:', error)
+      }
+    )
+  }
 }
