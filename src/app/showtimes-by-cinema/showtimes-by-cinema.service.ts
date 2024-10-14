0c3d9805127decc41e221ddcda7fedaee70dcffe
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-10-14 11:46:35 -0300

    crud showtimes terminado hasta la parte de listar las funciones de ese cine, falta editar, agregar, actualizar, eliminar, y controlar que esa funcion pertenesca a ese cine

--- src/app/showtimes-by-cinema/showtimes-by-cinema.service.ts
+++ src/app/showtimes-by-cinema/showtimes-by-cinema.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cinema, ResponseList, ResponseOne, ResponseWithError, Show } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowtimesByCinemaService {
 
-  constructor() { }
+  readonly showtimeUrl = "http://localhost:3000/api/shows"
+  readonly cinemaUrl = "http://localhost:3000/api/cinemas"
+  constructor(private http :HttpClient) { }
+
+  getCinema(id :number): Observable<any>{
+    return this.http.get<ResponseOne<Cinema> | ResponseWithError>(`${this.cinemaUrl}/${id}`);
+  }
+  getShowtimesByCinema(id :number): Observable<any>{
+    return this.http.get<ResponseList<Show> | ResponseWithError>(`${this.showtimeUrl}/bycinema/${id}`);
+  }
 }
