78274d5420318c35aa6854986c7e72ed72b7246c
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-10-15 23:26:44 -0300

    crud funciones completo

--- src/app/showtimes-by-cinema/showtimes-by-cinema.service.ts
+++ src/app/showtimes-by-cinema/showtimes-by-cinema.service.ts
@@ -1,42 +1,70 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Cinema, Movie, ResponseList, ResponseOne, ResponseWithError, Show } from '../interfaces/interfaces';
+import {
+  Cinema,
+  Movie,
+  ResponseList,
+  ResponseOne,
+  ResponseWithError,
+  Show,
+} from '../interfaces/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShowtimesByCinemaService {
+  readonly showtimeUrl = 'http://localhost:3000/api/shows';
+  readonly movieUrl = 'http://localhost:3000/api/movies';
+  readonly cinemaUrl = 'http://localhost:3000/api/cinemas';
+  constructor(private http: HttpClient) {}
 
-  readonly showtimeUrl = "http://localhost:3000/api/shows"
-  readonly movieUrl = "http://localhost:3000/api/movies"
-  readonly cinemaUrl = "http://localhost:3000/api/cinemas"
-  constructor(private http :HttpClient) { }
-
-  getCinema(id :number): Observable<any>{
-    return this.http.get<ResponseOne<Cinema> | ResponseWithError>(`${this.cinemaUrl}/${id}`);
+  getCinema(id: number): Observable<any> {
+    return this.http.get<ResponseOne<Cinema> | ResponseWithError>(
+      `${this.cinemaUrl}/${id}`
+    );
   }
 
-  getShowtimesByCinema(id :number): Observable<any>{
-    return this.http.get<ResponseList<Show> | ResponseWithError>(`${this.showtimeUrl}/bycinema/${id}`);
+  getShowtimesByCinema(id: number): Observable<any> {
+    return this.http.get<ResponseList<Show> | ResponseWithError>(
+      `${this.showtimeUrl}/bycinema/${id}`
+    );
   }
 
-  getShowtime(id: number,moreDetails :string | null = null): Observable<any>{
-    if(moreDetails != null && moreDetails?.toLocaleLowerCase() == "yes"){
-      return this.http.get<ResponseOne<Show> | ResponseWithError>(`${this.showtimeUrl}/${id}?moredetails=${moreDetails}`);
+  getShowtime(id: number, moreDetails: string | null = null): Observable<any> {
+    if (moreDetails != null && moreDetails?.toLocaleLowerCase() == 'yes') {
+      return this.http.get<ResponseOne<Show> | ResponseWithError>(
+        `${this.showtimeUrl}/${id}?moredetails=${moreDetails}`
+      );
     }
-      return this.http.get<ResponseOne<Show> | ResponseWithError>(`${this.showtimeUrl}/${id}`);
+    return this.http.get<ResponseOne<Show> | ResponseWithError>(
+      `${this.showtimeUrl}/${id}`
+    );
   }
 
-  getMovieData(id :number): Observable<any>{
-    return this.http.get<ResponseOne<Movie> | ResponseWithError>(`${this.movieUrl}/${id}`);
+  getMovieData(id: number): Observable<any> {
+    return this.http.get<ResponseOne<Movie> | ResponseWithError>(
+      `${this.movieUrl}/${id}`
+    );
   }
 
-  updateShowtime(id: number, show : Show): Observable<any>{
-    return this.http.put<ResponseOne<Show> | ResponseWithError>(`${this.showtimeUrl}/${id}`,show);
+  addShowtime(show: Show): Observable<any> {
+    return this.http.post<ResponseOne<Show> | ResponseWithError>(
+      this.showtimeUrl,
+      show
+    );
   }
 
-  deleteShowtime(id: number): Observable<any>{
-    return this.http.delete<ResponseOne<Show> | ResponseWithError>(`${this.showtimeUrl}/${id}`);
+  updateShowtime(id: number, show: Show): Observable<any> {
+    return this.http.put<ResponseOne<Show> | ResponseWithError>(
+      `${this.showtimeUrl}/${id}`,
+      show
+    );
+  }
+
+  deleteShowtime(id: number): Observable<any> {
+    return this.http.delete<ResponseOne<Show> | ResponseWithError>(
+      `${this.showtimeUrl}/${id}`
+    );
   }
 }
