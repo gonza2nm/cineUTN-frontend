fac9ec33e14071823ae9a62a7d0dc41c470d1cf4
Santino S <santinosimbel@gmail.com>
2024-10-29 19:27:30 -0300

    Visualizacion de link de imagen en tiempo real

--- src/app/movie-edit/movie-edit.component.ts
+++ src/app/movie-edit/movie-edit.component.ts
@@ -15,6 +15,7 @@ export class MovieEditComponent implements OnInit {
   isEditMode: boolean = false;
   errorMessage: string | null = null;
   movieForm: FormGroup;
+  imageLink: string = '';
   movieData: Movie = {
     id: 0,
     name: '',
@@ -31,13 +32,22 @@ export class MovieEditComponent implements OnInit {
     private movieService: MovieService,
     private router: Router //permite redirigir a una página diferente despues de que se haya completado alguna acción. (Ej: luego de crear la pelicula lo mando a la lista de peliculas)
   ) {
+
     //se inicializa dentro del constructor, para que este configurado y disponible para usarse
     this.movieForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       description: new FormControl('', [Validators.required]),
       imageLink: new FormControl('', [Validators.required]),
     });
+
+    // Actualiza imageLink en tiempo real
+    this.movieForm.get('imageLink')?.valueChanges.subscribe((value: string) => {
+      this.imageLink = value;
+    });
+
   }
+
+
 
   ngOnInit(): void {
     //recupero el id de la ruta actual
@@ -60,4 +70,7 @@ export class MovieEditComponent implements OnInit {
   deleteMovie() {
 
   }
+
+
+
 }
