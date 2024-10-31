441777fb54b8d42e47ea6cafb55b3698ed3e968c
Santino S <santinosimbel@gmail.com>
2024-10-31 18:52:39 -0300

    Format service creado

---
+++ src/app/formats/format.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Format, ResponseList, ResponseWithError } from '../interfaces/interfaces';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormatService {
+
+  readonly urlFormats = 'http://localhost:3000/api/formats ';
+
+  //HttpClient Se inyecta en el servicio a través del constructor para que pueda usarse dentro de los métodos del servicio
+  constructor(private http: HttpClient) { }
+
+
+  getFormats(): Observable<any> {
+    return this.http
+      .get<ResponseList<Format> | ResponseWithError>(this.urlFormats)
+  }
+
+
+
+}
+
