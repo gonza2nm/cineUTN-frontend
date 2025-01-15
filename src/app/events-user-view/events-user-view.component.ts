020ba5b51ef927712d4a5a1c63d52eeb3e30a526
Santino S <santinosimbel@gmail.com>
2025-01-15 18:49:38 -0300

    Events-user-view funcionando

--- src/app/events-user-view/events-user-view.component.ts
+++ src/app/events-user-view/events-user-view.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../events/event.service';
+import { Event } from '../interfaces/interfaces.js';
 
 @Component({
   selector: 'app-events-user-view',
   templateUrl: './events-user-view.component.html',
   styleUrls: ['./events-user-view.component.css']
 })
-export class EventsUserViewComponent {
+export class EventsUserViewComponent implements OnInit {
+
+  events: Event[] = [];
+  errorMessage: string | null = null;
+  loading: boolean = true
+  constructor(private eventService: EventService) { }
+
+  ngOnInit(): void {
+    this.loadAllEvents()
+  }
+
+  loadAllEvents() {
+    this.eventService.getAllEvents().subscribe({
+      next: (response) => {
+        this.events = response.data;
+        this.errorMessage = null;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.errorMessage = 'Ocurrio un error buscando los eventos, intente nuevamente.';
+        console.error('Error getting events:', err.error.message);
+        this.loading = false;
+      }
+    });
+  }
+
+  getCinemasNames(event: Event): string {
+    // Agarramos los nombres de los cines de ese evento
+    return event.cinemas.map(cinema => cinema.name).join(', ');
+  }
 
 }
