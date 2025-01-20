c3da150675219454ed4e805fc160d6c9f3e7ff3b
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2025-01-20 06:20:25 -0300

    correccion del authGeneralGuard

--- src/app/auth/authGeneral.guard.ts
+++ src/app/auth/authGeneral.guard.ts
@@ -7,8 +7,16 @@ export const AuthGeneralGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
    if(authService.isUser() || authService.isManager()){
+    router.navigate(['/']);
     return true;
    }else{
-    return authService.checkCookieAndPermissions(null);
+    const permissons = authService.checkCookieAndPermissions(null);
+    if (!permissons){
+      router.navigate(['/login']);
+      return false;
+    }else{
+      router.navigate(['/']);
+      return true;
+    }
    }
 };
