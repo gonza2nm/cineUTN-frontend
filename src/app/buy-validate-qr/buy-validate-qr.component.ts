4cdadda13f06e3a50948fd28f7b6b92d54b3b85a
Santino S <santinosimbel@gmail.com>
2025-01-22 16:01:03 -0300

    Detener camara al salir del componente

--- src/app/buy-validate-qr/buy-validate-qr.component.ts
+++ src/app/buy-validate-qr/buy-validate-qr.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Buy } from '../interfaces/interfaces.js';
 import { BuyService } from '../buy/buy.service';
-import { BrowserQRCodeReader } from '@zxing/browser';
+import { BrowserQRCodeReader, IScannerControls } from '@zxing/browser';
 
 @Component({
   selector: 'app-buy-validate-qr',
   templateUrl: './buy-validate-qr.component.html',
   styleUrls: ['./buy-validate-qr.component.css']
 })
-export class BuyValidateQrComponent {
+export class BuyValidateQrComponent implements OnDestroy {
 
   errorMessage: string | null = null;
   buyData: Buy = {
@@ -31,20 +31,24 @@ export class BuyValidateQrComponent {
 
   }
 
+  private scannerControls: IScannerControls | null = null;
+
   constructor(private buyService: BuyService) { }
 
   scanQRCode() {
     const codeReader = new BrowserQRCodeReader(); //viene de @zxing/browser
 
     codeReader.decodeFromVideoDevice(undefined, 'video',
-      (result, error) => {
+      (result, error, controls) => {
+        if (controls) {
+          this.scannerControls = controls; //guardamos los controles para poder detener la camara luego
+        }
         if (result) {
           const token = result.getText(); // consigue el texto del qr
           this.validateQRCode(token); // Valida el QR con el backend
         }
         if (error) {
-          this.errorMessage = 'No se pudo leer el QR. Intenta nuevamente.';
-          console.error('Error scanning QR code:', error);
+          this.errorMessage = 'No se detecta el QR. Intenta enfocarlo mejor.';
         }
       }
     )
@@ -57,9 +61,16 @@ export class BuyValidateQrComponent {
         this.errorMessage = null;
       },
       error: (error) => {
-        this.errorMessage = 'El QR no es válido o ha expirado.';
-        console.error('Error al validar el QR:', error);
+        this.errorMessage = 'El QR no es válido o ha expirado. Intente recargando el detalle de la compra';
+        console.error('Error validating QR:', error);
       },
     });
+  }
+
+  ngOnDestroy() { //mata la camara cuando salismos del componente
+    if (this.scannerControls) {
+      this.scannerControls.stop(); // detenemos la camara
+      console.log('Cámara detenida al salir del componente.');
+    }
   }
 }
