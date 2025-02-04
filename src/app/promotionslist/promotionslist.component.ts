4da6ef33d8335569f268ba23177cd460cfc336bb
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2025-02-04 11:05:10 -0300

    correccion de codigo innecesario en promotionlist

--- src/app/promotionslist/promotionslist.component.ts
+++ src/app/promotionslist/promotionslist.component.ts
@@ -13,47 +13,23 @@ export class PromotionslistComponent implements OnInit {
   promotions: Promotion[] = [];
   errorMessage: string | null = null;
   loading: boolean = true;
-  cinemas: Cinema[] = [];
-  filterbyCinema: Promotion[] = [];
 
   constructor(
     private promotionService: PromotionsService,
-    private cinemaService: CinemaService
   ) {}
   ngOnInit(): void {
     this.loadPromotions();
-    this.loadCinemas();  
   }
 
   loadPromotions() {
       this.promotionService.getAllPromotions().subscribe({
         next: (response) => {
           this.promotions = response.data;
-          this.filterbyCinema = this.promotions;
           this.errorMessage = null;
           this.loading = false;
         },
         error: (err) => {
           this.errorMessage = 'Ocurrio un error al buscar las promociones, intente nuevamente.';
-          console.error('Error getting promotions:', err.error.message);
-          this.loading = false;
-        }
-      })
-    }
-  
-    loadCinemas() {
-      this.cinemaService.getAllCinemas().subscribe({
-        next: (response) => {
-          this.cinemas = response.data.map((cinema: Cinema) => ({
-          id: cinema.id,
-          name: cinema.name,
-          addres: cinema.address
-        }));
-          this.errorMessage = null;
-          this.loading = false;
-        },
-        error: (err) => {
-          this.errorMessage = 'Ocurrio un error al buscar los cines, intente nuevamente.';
           console.error('Error getting promotions:', err.error.message);
           this.loading = false;
         }
