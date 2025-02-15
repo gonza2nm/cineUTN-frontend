383e794fbb7f101288c811dbd51fc4872ef82c7c
Santino S <santinosimbel@gmail.com>
2025-02-15 16:40:38 -0300

    arreglo de fechas en edit events, mejora de formato de fecha.

--- src/app/event-edit/event-edit.component.ts
+++ src/app/event-edit/event-edit.component.ts
@@ -165,14 +165,15 @@ export class EventEditComponent implements OnInit {
 
   formatToDateTimeLocal(date2: Date): string {
     const date = new Date(date2);
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-    const hours = ('0' + date.getHours()).slice(-2);
-    const minutes = ('0' + date.getMinutes()).slice(-2);
+    // Ajustar la fecha eliminando la diferencia de zona horaria
+    const year = date.getUTCFullYear();
+    const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+    const day = ('0' + date.getUTCDate()).slice(-2);
 
-    return `${year}-${month}-${day}T${hours}:${minutes}`;
+    return `${year}-${month}-${day}`;
   }
+
+
 
   toggleCinemaSelection(cinemaId: number) {
     const index = this.eventCinemasIds.indexOf(cinemaId);
